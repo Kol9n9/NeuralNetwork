41fedc019d0cbfebbc480f5e181142b9f7fc093b
NikolayS <slezenkonikolay@mail.ru>
2022-05-15 22:15:52 +0700

    initial

---
+++ NeuralNetwork/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Start reading from file");
+            List<Anketa> anketas = Reader.ReadFromExcel("Ankety.csv");
+            Console.WriteLine($"Read is end. Readed {anketas.Count}");
+            Console.WriteLine("Start prepare data set");
+            var dataset = PrepareDataSet(anketas);
+            Console.WriteLine("Data set is prepared");
+            Console.WriteLine("Creating Neural Network");
+            NeuronTopology topology = new NeuronTopology(32, 1, 8);
+            NeuralNetwork neuralNetwork = new NeuralNetwork(topology);
+            Console.WriteLine("Neural Network is created");
+            Console.WriteLine("Start learning");
+            double error = neuralNetwork.Learn(dataset, 1000);
+            Console.WriteLine("Learning is end");
+            for (int i = 0; i < anketas.Count; i++)
+            {
+                double result = neuralNetwork.FeedForward(dataset[i].Item2).Output;
+                Console.WriteLine($"{i} {anketas[i].IsSerious} - {result}");
+            }
+
+        }
+        public static List<Tuple<double,double[]>> PrepareDataSet(List<Anketa> anketas)
+        {
+            List<Tuple<double, double[]>> dataset = new List<Tuple<double, double[]>>();
+
+            foreach(var anket in anketas)
+            {
+                List<double> answers = new List<double>();
+                answers.AddRange(anket.Authoritarian);
+                answers.AddRange(anket.Selfish);
+                dataset.Add(new Tuple<double, double[]>(anket.IsSerious, answers.ToArray()));
+            }
+            return dataset;
+        }
+    }
+}
---
+++ NeuralNetwork/Reader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    public static class Reader
+    {
+        public static List<Anketa> ReadFromExcel(string filepath)
+        {
+            List<Anketa> anketas = new List<Anketa>();
+            using(var reader = new StreamReader(filepath))
+            {
+                string line;
+                bool isHeader = false;
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                    if (!isHeader)
+                    {
+                        isHeader = true;
+                        continue;
+                    }
+                    anketas.Add(ParseLine(line));
+                }
+            }
+            return anketas;
+        }
+        private static Anketa ParseLine(string text)
+        {
+            string[] rows = text.Split(new char[] { ';' });
+            double term = double.Parse(rows[1]);
+            List<double> answers = new List<double>();
+            for(int i = 2; i < rows.Length; i++)
+            {
+                answers.Add(double.Parse(rows[i]));
+            }
+            Anketa anketa = new Anketa();
+            anketa.IsSerious = term > 7 ? 1 : 0;
+            anketa.Authoritarian = GroupAnswer(answers, "1-4", "33-36", "65-68", "97-100");
+            anketa.Selfish = GroupAnswer(answers, "5-8", "37-40", "69-72", "101-104");
+            anketa.Aggresive = GroupAnswer(answers, "9-12", "41-44", "73-76", "105-108");
+            anketa.Suspicious = GroupAnswer(answers, "13-16", "45-48", "77-80", "109-112");
+            anketa.Subordinate = GroupAnswer(answers, "17-20", "49-52", "81-84", "113-116");
+            anketa.Dependent = GroupAnswer(answers, "21-24", "53-56", "85-88", "117-120");
+            anketa.Friendly = GroupAnswer(answers, "25-28", "57-60", "89-92", "121-124");
+            anketa.Altruistic = GroupAnswer(answers, "29-32", "61-64", "93-96", "125-128");
+            return anketa;
+        }
+        private static double[] GroupAnswer(List<double> answers, params string[] groups)
+        {
+            List<double> groupedAnswer = new List<double>();
+            for(int i = 0; i < groups.Length; i++)
+            {
+                string group = groups[i];
+                string[] groupValue = group.Split(new char[] { '-' });
+                int start = int.Parse(groupValue[0]);
+                int finish = int.Parse(groupValue[1]);
+                groupedAnswer.AddRange(answers.Skip(start - 1).Take(finish - start + 1));
+            }
+            return groupedAnswer.ToArray();
+        }
+
+    }
+}
